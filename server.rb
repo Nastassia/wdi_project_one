f0809f477c831da83e7ea65d7a2c29686f8d8c28
Nastassia <nastassiacarmona@gmail.com>
2014-09-05 00:48:50 -0400

    Finish edit authors functionality, and add Delete authors functionality

--- server.rb
+++ server.rb
@@ -45,6 +45,28 @@ end
 
 get "/authors/show/:id/edit" do
   author = Author.find_by(id: params["id"])
-
   erb(:"/authors/edit", locals: {author: author})
+end
+
+put "/authors" do
+  name = params["name"]
+  user_name = params["user_name"]
+  bio = params["bio"]
+  picture = params["picture"]
+  phonenum = params["phoneNum"]
+  email = params["email"]
+
+  author = Author.find_by({name: params["name"]})
+
+  update_author = {name: name, user_name: user_name, bio: bio, picture: picture, phonenum: phonenum, email: email}
+
+  author.update(update_author)
+
+  erb(:"/authors/index", locals: { authors: Author.all() })
+end
+
+delete "/authors/:id" do
+  author = Author.find_by({id: params["id"]})
+  author.destroy
+  redirect "/authors"
 end
